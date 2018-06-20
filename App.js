a7ac31acb6f8f68cfda8e873c9fcc31a4441647c
Zachary Haynie <zacharyrhaynie@gmail.com>
2018-06-20 10:47:54 -0600

    Initial Commit. MVP

---
+++ App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
+
+export default class App extends React.Component {
+    state = {
+        bill: '20.00',
+        people: '2',
+        tip: '20'
+    };
+
+    render() {
+        const SubmitHandler = (event) => {
+            event.preventDefault();
+            alert(`You're tipping $${TipAmount()} which brings the total to $${BillAmount()}. This means each person pays $${AmountToPay()}`)
+        };
+        let bill = (Number.parseFloat(this.state.bill, 10));
+        let people = (Number.parseInt(this.state.people, 10));
+        let tip = (Number.parseFloat((this.state.tip / 100), 10));
+
+        const TipAmount = () => {
+            return (Number.parseFloat((bill * tip).toFixed(2)))
+        };
+
+        const BillAmount = () => {
+            return (Number.parseFloat((bill + TipAmount()).toFixed(2)))
+        };
+
+        const AmountToPay = () => {
+            return (Number.parseFloat((BillAmount() / people).toFixed(2)))
+        };
+        return (
+            <View style={styles.container}>
+                <Text>Welcome to Basic Split a Bill!</Text>
+                <Text>How much is the bill?</Text>
+                <TextInput keyboardType="numeric"
+                           defaultValue="20.00"
+                           onChangeText={(bill) => this.setState({bill})}
+                />
+                <Text>How many people are with you?</Text>
+                <TextInput keyboardType="numeric"
+                           defaultValue="2"
+                           onChangeText={(people) => this.setState({people})}
+                />
+                <Text>What percent are you tipping?</Text>
+                <TextInput keyboardType="numeric"
+                           defaultValue="20"
+                           onChangeText={(tip) => this.setState({tip})} onSubmitEditing={SubmitHandler}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
